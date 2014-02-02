0a9de501ead5e51272d06e5b7f6f7f38600717e6
Jens Christian Hillerup <jens@hillerup.net>
2014-02-02 17:07:26 +0100

    adding a script that makes orders

---
+++ tests/mkorders.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from random import randint, choice
+import requests
+from time import sleep
+
+kitchens = ['GL', 'ML', 'NY']
+
+
+LINES = 20000
+now = 1391114661
+
+for i in range(LINES):
+    r = requests.get('http://localhost:8081/?mode=streg&kitchen=%s%d&quantity=%d' % (choice(kitchens), randint(2,8), (randint(2,12) +0.0) / 2)) 
+
+    print r.text
+    
+    sleep(7)
+    
+    #print "%d;%s%d;%d" % (now, choice(kitchens), randint(2, 8), (randint(2, 12) / 2) )
+
+    now += randint(1, 10)
