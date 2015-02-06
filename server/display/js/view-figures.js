8a188b4cdae1e4e4cfc77e013b6d3ab885b47bce
Jens Christian Hillerup <jens@hillerup.net>
2015-02-06 14:35:57 +0100

    new plots view

--- server/display/js/view-figures.js
+++ server/display/js/view-figures.js
@@ -1,8 +1,8 @@
 var PlotsView = Backbone.View.extend({
     id: "plots",
-    num_plots: 14,
     cur_plot: 0,
     $cur_img: null,
+    plotfiles: ['plot0.png', 'plot10.png', 'plot11.png', 'plot12.png', 'plot13.png', 'plot1.png', 'plot2.png', 'plot4.png', 'plot5.png', 'plot6.png', 'plot7.png', 'plot9.png'],
 
     events: {
         "click img": "advance"
@@ -14,25 +14,25 @@ var PlotsView = Backbone.View.extend({
         this.insertPic();
         this.insertPic();
         this.advance();
-        setInterval(_.bind(this.advance, this), 25000);
+
+        setInterval(_.bind(this.advance, this), 18000);
     },
     
     insertPic: function() {
-        var path = "plots/plot"+(this.cur_plot + 1)+".png?cachebust="+(new Date()).getTime();
+        var path = "plots/"+(this.plotfiles[this.cur_plot + 1])+"?cachebust="+(new Date()).getTime();
         var $img = $("<img>").attr('src', path).css('left', '480px');
 
-        this.$el.append($img); 
+        this.$el.append($img);
         this.$cur_img = $img;
     },
 
     advance: function(event) {
         this.cur_plot += 1;
-        this.cur_plot %= this.num_plots;
+        this.cur_plot %= len(this.plotfiles);
         
         this.insertPic();
 
         this.$("img:first").animate({"left": "-480px"}, 600, function() {$(this).remove();});
-        $(this.$("img").get(1)).animate({"left": "0px"}, 600);
-        
+        $(this.$("img").get(1)).animate({"left": "0px"}, 600);       
     }
 });
